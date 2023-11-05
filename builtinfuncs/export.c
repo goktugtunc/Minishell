0e72213fd5c1ef4e735b41295ce0ba4f7da501c4
goktugtunc <goktugtunc@goktugtunc-Mac.local>
2023-11-04 17:36:56 -0700

    isupdate fonksiyonu ile yeni bir değer eklenip eklenmedigini anlayabiliriz

--- builtinfuncs/export.c
+++ builtinfuncs/export.c
@@ -6,7 +6,7 @@
 /*   By: goktugtunc <goktugtunc@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 11:13:15 by gotunc            #+#    #+#             */
-/*   Updated: 2023/11/04 15:52:47 by goktugtunc       ###   ########.fr       */
+/*   Updated: 2023/11/04 17:36:02 by goktugtunc       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 /*
 	TODO
 	Exportla ekleyip sıraladığında bazen garbage değer atıyor.
-	Muhtemelen NULL ataması ile ilgili.
+	Yeni değer eklerken ilk pointer a yer ataması yapmıyorum. Oyüzden de garbage değer atıyor ve kodda hataya sebep oluyor.
+	Şuanda da 2 kere aynı şeyi yazdığımda bir alan açmadığı için segment yiyorum freelerken.
 */
 
 void	sortexport(void)
@@ -44,6 +45,37 @@ void	sortexport(void)
 	}
 }
 
+void	isupdate(char **str, int j)
+{
+	int	i;
+
+	i = 0;
+	i = 0;
+	if (!ft_strchr(str[j], '='))
+	{
+		while (g_data->exportp[i])
+		{
+			if (ft_strcmp(g_data->exportp[i], str[j]) == 0)
+				free(g_data->exportp[i]);
+			i++;
+		}
+		return ;
+	}
+	while (g_data->exportp[i])
+	{
+		if (ft_strcmp(ft_split(str[j], '=')[0], ft_split(g_data->exportp[i], '=')[0]) == 0)
+			free(g_data->exportp[i]);
+		i++;
+	}
+	i = 0;
+	while (g_data->envp[i])
+	{
+		if (ft_strcmp(ft_split(str[j], '=')[0], ft_split(g_data->envp[i], '=')[0]) == 0)
+			free(g_data->envp[i]);
+		i++;
+	}
+}
+
 void	exportcommand(char **str) // fonksiyonda string i sıralayacak bir algoritma yazmam gerekiyor. Bir de parse yapılacak. Girilen parametrenin başında sayı olmayacak . olmayacak gibi
 {
 	int	i;
@@ -55,12 +87,13 @@ void	exportcommand(char **str) // fonksiyonda string i sıralayacak bir algoritm
 		i = 1;
 		while (i < commandpointerlen(str))
 		{
+			isupdate(str, i);
 			if (ft_strchr(str[i], '='))
 			{
 				// g_data->envp[commandpointerlen(g_data->envp) + 1] = NULL;
 				g_data->envp[commandpointerlen(g_data->envp)] = ft_strdup(str[i]);
 			}
-			g_data->exportp[g_data->exportlen + 1] = NULL;
+			// g_data->exportp[g_data->exportlen + 1] = NULL;
 			g_data->exportp[g_data->exportlen] = ft_strdup(str[i]);
 			g_data->exportlen++;
 			i++;
