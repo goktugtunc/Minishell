76178951d7ecba12f00a2fcfdbd0a0527a4ba59a
Goktug Tunc <gotunc@k1m39s03.42istanbul.com.tr>
2023-11-18 16:35:08 +0300

    control c calisir duruma getirildi

--- signals.c
+++ signals.c
@@ -6,7 +6,7 @@
 /*   By: gotunc <gotunc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 09:16:22 by gotunc            #+#    #+#             */
-/*   Updated: 2023/11/18 14:23:21 by gotunc           ###   ########.fr       */
+/*   Updated: 2023/11/18 16:30:53 by gotunc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,11 @@ void	ifsendsigquit(int signal)
 void	ifsendsigint(int signal)
 {
 	(void)signal;
+	rl_replace_line("", 0);
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
 void	ifsendeof(t_data *data)
